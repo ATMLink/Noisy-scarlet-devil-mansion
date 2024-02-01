2b6e505b88c23fadcef518590e64e8ccbfc2d1f9
ATMLink <2937582423@qq.com>
2024-02-01 20:20:11 +0800

    create card object and card data

---
+++ Assets/Scripts/CardObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Text;
+
+public class CardObject : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro costText;
+    [SerializeField] private TextMeshPro nameText;
+    [SerializeField] private TextMeshPro typeText;
+    [SerializeField] private TextMeshPro descriptionText;
+
+    [SerializeField] private SpriteRenderer illustration;
+
+    public CardTemplate template;
+    public RuntimeCard runtimeCard;
+
+    private void Start()
+    {
+        var testCard = new RuntimeCard
+        {
+            template = template
+        };
+        SetInfo(testCard);
+    }
+    public void SetInfo(RuntimeCard card)
+    {
+        runtimeCard = card;
+        template = card.template;
+        costText.text = template.cost.ToString();
+        nameText.text = template.name;
+        typeText.text = template.type.typeName;
+        var builder = new StringBuilder();
+        descriptionText.text = builder.ToString();
+        illustration.sprite = template.illustration;
+    }
+}
