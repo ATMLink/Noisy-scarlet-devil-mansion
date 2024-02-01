3e7e00f74b05628a746b6974eec30c6b5cefefab
ATMLink <2937582423@qq.com>
2024-02-01 18:33:02 +0800

    basic card type data

---
+++ Assets/Scripts/CardTemplate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "CardGame/Templates/Card", order = 0)]
+public class CardTemplate : ScriptableObject
+{
+    public int id;
+    public string name;
+    public int cost;
+    public Sprite illustration;
+    public CardType type;
+}
---
+++ Assets/Scripts/CardType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardType", menuName ="CardGame/Template/Card Type", order = 0)]
+public class CardType : ScriptableObject
+{
+    public string typeName;
+}
