85b1edd0da84f4be47f51f9ca283fd145d47ed46
ATMLink <2937582423@qq.com>
2024-02-01 16:59:41 +0800

    card prefab and cards manager

---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CardsManager : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public int size;
+
+    private readonly Stack<GameObject> _instances = new Stack<GameObject>();
+
+    private void Awake()
+    {
+        Assert.IsNotNull(cardPrefab);
+    }
+
+    public void initialize()
+    {
+        for(int i = 0; i < size; i++)
+        {
+            var obj = createInstance();//创建实例化cardobject。
+            obj.SetActive(false);
+            _instances.Push(obj);
+        }
+    }
+
+    private GameObject createInstance()
+    {
+        var cardObject = Instantiate(cardPrefab, transform, true);
+        return cardObject;
+    }
+}
