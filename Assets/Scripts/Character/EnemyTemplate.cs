efa4de282e218a6b22f25f42a745993f174e0b1a
ATMLink <2937582423@qq.com>
2024-02-07 20:53:58 +0800

    character create

---
+++ Assets/Scripts/Character/EnemyTemplate.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName = "CardGame/Templates/ ")]
+public class EnemyTemplate : MonoBehaviour
+{
+    
+}
