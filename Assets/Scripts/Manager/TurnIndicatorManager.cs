0453979791901e664b584df7dcd7a0c8f6cdd1bb
ATMLink <2937582423@qq.com>
2024-02-18 11:19:27 +0800

    turn indicator

--- Assets/Scripts/Manager/TurnIndicatorManager.cs
+++ Assets/Scripts/Manager/TurnIndicatorManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -7,4 +8,26 @@ public class TurnIndicatorManager : MonoBehaviour
 {
     [SerializeField] public GameObject turnIndicator;
     [SerializeField] public TextMeshProUGUI text;
+
+    private const float InAnimDuration = 0.5f;
+    private const float OutAnimDuration = 0.6f;
+    private const float ShowcaseDuration = 2.0f;
+
+    public void OnPlayerTurnBegan()
+    {
+        text.text = "Player Turn";
+        var seq = DOTween.Sequence();
+        seq.Append(turnIndicator.GetComponent<RectTransform>().DOScale(1f, InAnimDuration));
+        seq.AppendInterval(ShowcaseDuration);
+        seq.Append(turnIndicator.GetComponent<RectTransform>().DOScale(0.0f, OutAnimDuration));
+    }
+    
+    public void OnEnemyTurnBegan()
+    {
+        text.text = "Enemy Turn";
+        var seq = DOTween.Sequence();
+        seq.Append(turnIndicator.GetComponent<RectTransform>().DOScale(1f, InAnimDuration));
+        seq.AppendInterval(ShowcaseDuration);
+        seq.Append(turnIndicator.GetComponent<RectTransform>().DOScale(0.0f, OutAnimDuration));
+    }
 }
