665028983ca68f6805f105eb02c5c57faf7af3f2
ATMLink <2937582423@qq.com>
2024-03-29 13:51:33 +0800

    god punishment developing

---
+++ Assets/Scripts/Effect/DealAOEDamageEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CardGame/Effects/IntegerEffect/Deal AOE Damage Effect",
+    fileName = "DealAOEDamageEffect",
+    order = 11)]
+public class DealAOEDamageEffect : MonoBehaviour
+{
+    
+}
